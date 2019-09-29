e97f353074338e74b008ad1837b22f7493cebe46
Christian Smith <christian.smith0016@gmail.com>
2019-09-29 15:40:21 -0400

    Update GitHubApi.py

--- GitHubApi.py
+++ GitHubApi.py
@@ -4,14 +4,15 @@ Created on Sep 22, 2019
 @author: Christian Smith
 '''
 import unittest
+from unittest import mock
 from github import Github
+import GitHubApi
 
 def getRepositories(user_id):
     if(' ' in user_id or len(user_id) == 0):
         print(user_id + ' is an invalid username')
         return 'invalid'
     else:
-        g = Github("f26a21eb3a5288199012753649a52a9b5bed8bc9")
         print('User: ' + user_id)
         user = g.get_user(user_id)
         for repo in user.get_repos():
@@ -20,18 +21,21 @@ def getRepositories(user_id):
             except:
                 print('EMPTY REPO')
     return 'valid'
-            
-    
+                
 class Test(unittest.TestCase):
+
+    @mock.patch('GitHubApi.getRepositories', return_value='invalid')
+
+    def testInvalid(self, *args):  # @DontTrace
+        self.assertIs(GitHubApi.getRepositories('j24 12'), 'invalid', 'User ID with spaces is invalid')
+        self.assertIs(GitHubApi.getRepositories(''), 'invalid', 'Empty User ID is invalid')
     
-    def testInvalid(self):
-        self.assertIs(getRepositories('j24 12'), 'invalid', 'User ID with spaces is invalid')
-        self.assertIs(getRepositories(''), 'invalid', 'Empty User ID is invalid')
+    @mock.patch('GitHubApi.getRepositories', return_value='valid')
         
-    def testValid(self):
-        self.assertIs(getRepositories('Dioden'), 'valid', 'User ID should be valid')
-        self.assertIs(getRepositories('google'), 'valid', 'User ID should be valid')
-        self.assertIs(getRepositories('amzn'), 'valid', 'User ID should be valid')
+    def testValid(self, *args):  # @DontTrace
+        self.assertIs(GitHubApi.getRepositories('Dioden'), 'valid', 'User ID should be valid')
+        self.assertIs(GitHubApi.getRepositories('google'), 'valid', 'User ID should be valid')
+        self.assertIs(GitHubApi.getRepositories('amzn'), 'valid', 'User ID should be valid')
 
 
 if __name__ == "__main__":
